9b618bed5c8f17bd8c1dba5dab6e143ed600ec01
gudian1618 <1761844211@qq.com>
2021-04-06 20:58:42 +0800

    Initial commit

---
+++ src/test/java/com/github/gudian1618/cgb2011activity01/Cgb2011activity01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gudian1618.cgb2011activity01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgb2011activity01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
