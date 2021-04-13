734bf520406fb19e9906bfec77c2cc2ee051dca1
gudian1618 <1761844211@qq.com>
2021-04-13 22:57:23 +0800

    new

--- src/main/java/com/github/gudian1618/cgb2011activity01/controller/ActivityController.java
+++ src/main/java/com/github/gudian1618/cgb2011activity01/controller/ActivityController.java
@@ -23,35 +23,52 @@ public class ActivityController {
     @Autowired
     private ActivityService activityService;
 
-    @RequestMapping("activity_edit")
-    public String doActivityEditUI(Model model) {
-        model.addAttribute("act", new Activity());
-        return "activity_edit";
-    }
-
-    @RequestMapping("doFindById")
-    public String doFindById(Long id, Model model) {
-        Activity act = activityService.findById(id);
-        model.addAttribute("act", act);
-        return "activity_edit";
-    }
-
     @RequestMapping("doSaveActivity")
+    @ResponseBody
     public String doSaveActivity(Activity entity) {
-        if (entity.getId() == null) {
-            activityService.saveActivity(entity);
-        } else {
-            activityService.updateActivity(entity);
-        }
-        return "redirect:activity_list";
+        activityService.saveActivity(entity);
+        return "save ok";
+    }
+
+    @RequestMapping("activity_edit")
+    public String doActivityEditUI() {
+        return "activity_edit";
     }
 
     @RequestMapping("activity_list")
-    public String doActivityUI(Model model) {
-        List<Activity> list = activityService.findActivitys();
-        model.addAttribute("list", list);
+    public String doActivityListUI(Model model) {
         return "activity_list";
     }
+
+    // @RequestMapping("activity_edit")
+    // public String doActivityEditUI(Model model) {
+    //     model.addAttribute("act", new Activity());
+    //     return "activity_edit";
+    // }
+
+    // @RequestMapping("doFindById")
+    // public String doFindById(Long id, Model model) {
+    //     Activity act = activityService.findById(id);
+    //     model.addAttribute("act", act);
+    //     return "activity_edit";
+    // }
+
+    // @RequestMapping("doSaveActivity")
+    // public String doSaveActivity(Activity entity) {
+    //     if (entity.getId() == null) {
+    //         activityService.saveActivity(entity);
+    //     } else {
+    //         activityService.updateActivity(entity);
+    //     }
+    //     return "redirect:activity_list";
+    // }
+
+    // @RequestMapping("activity_list")
+    // public String doActivityUI(Model model) {
+    //     List<Activity> list = activityService.findActivitys();
+    //     model.addAttribute("list", list);
+    //     return "activity_list";
+    // }
 
     @RequestMapping("doFindActivitys")
     // 告诉springmvc 将返回json格式的字符串
